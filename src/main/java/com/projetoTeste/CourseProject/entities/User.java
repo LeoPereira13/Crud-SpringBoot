854250237fad63c849751a02826ea4737e7ae95a
Leo Pereira <leo.dpsouza@gmail.com>
2023-07-21 16:26:43 -0300

    implementação da entidade Order parte 1

--- src/main/java/com/projetoTeste/CourseProject/entities/User.java
+++ src/main/java/com/projetoTeste/CourseProject/entities/User.java
@@ -1,12 +1,15 @@
 package com.projetoTeste.CourseProject.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,7 +24,14 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {		
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
